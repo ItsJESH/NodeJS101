384e790227612ea29347e2cd9768e711dd4f8996
ItsJESH <jenishpaghadalin018@gmail.com>
2025-01-07 14:00:13 +0530

    Added nodejs assignment1

--- node_a1.js
+++ node_a1.js
@@ -1,9 +1,60 @@
 const fs = require('fs');
 const { lchown } = require('fs/promises');
 
+
+// -------------------------------------------------------------------------------------------
+// Synchronous operations 
+// -------------------------------------------------------------------------------------------
+
+
 // 1. Create a folder space_project
 
 // fs.mkdirSync('space_project')
+
+
+// 2. Inside the folder, create a file log.txt and add the line "ISRO is planning Gaganyaan mission, ie an Indian crewed orbital spacecraft."
+
+// let data = 'ISRO is planning Gaganyaan mission, ie an Indian crewed orbital spacecraft.'
+// fs.writeFileSync('space_project/log.txt',data)
+
+
+// 3. Now, replace the line in the log.txt file with "ISRO has started working on Gaganyaan."
+
+// let d = 'ISRO has started working on Gaganyaan.'
+// fs.writeFileSync('space_project/log.txt',d)
+
+
+// 4. Append another line to the log.txt file " The current Aditya-L1 team of scientists is mentoring new talent for working on the Gaganyaan mission."
+
+// let t = '\nThe current Aditya-L1 team of scientists is mentoring new talent for working on the Gaganyaan mission.'
+// fs.appendFileSync('space_project/log.txt',t)
+
+
+// 5. Rename the log.txt file to update.txt
+// fs.renameSync('space_project/log.txt','space_project/update.txt')
+
+
+// 6. Read the update.txt file and display the data. Additionally print a line "We are excited" using console.log() after the read operation
+// let a = fs.readFileSync('space_project/update.txt','utf-8')
+// console.log(a)
+// console.log('We are excited')
+
+
+// 7. Delete the file update.txt
+// fs.unlinkSync('space_project/update.txt')
+
+
+// 8. Delete the folder space_project
+// fs.rmdirSync('space_project')
+
+
+// -------------------------------------------------------------------------------------------
+// Asynchronous operations 
+// -------------------------------------------------------------------------------------------
+
+
+// 1. Create a folder space_project
+
 // fs.mkdir('space_project',(err)=>{
 //     if(err) throw err;
 // })
@@ -12,9 +63,6 @@ const { lchown } = require('fs/promises');
 // 2. Inside the folder, create a file log.txt and add the line "ISRO is planning Gaganyaan mission, ie an Indian crewed orbital spacecraft."
 
 // let data = 'ISRO is planning Gaganyaan mission, ie an Indian crewed orbital spacecraft.'
-
-// fs.writeFileSync('space_project/log.txt',data)
-
 // fs.writeFile('space_project/log.txt',data,(err)=>{
 //     if(err) throw err;
 // })
@@ -24,9 +72,7 @@ const { lchown } = require('fs/promises');
 
 // let d = 'ISRO has started working on Gaganyaan.'
 
-// fs.writeFileSync('space_project/log.txt',d)
-
-// fs.writeFile('space_project/log.txt', data,(err)=>{
+// fs.writeFile('space_project/log.txt', d,(err)=>{
 //     if(err) throw err;
 // })
 
@@ -35,17 +81,13 @@ const { lchown } = require('fs/promises');
 
 // let t = '\nThe current Aditya-L1 team of scientists is mentoring new talent for working on the Gaganyaan mission.'
 
-// fs.appendFileSync('space_project/log.txt',t)
-
-// fs.appendFile('space_project/log.txt',data,(err)=>{
+// fs.appendFile('space_project/log.txt',t,(err)=>{
 //     if(err) throw err;
 // })
 
 
 
 // 5. Rename the log.txt file to update.txt
-// fs.renameSync('space_project/log.txt','space_project/update.txt')
-
 // fs.rename('space_project/log.txt','space_project/update.txt',(err)=>{
 //     if(err) {throw err;}
 // })
@@ -54,10 +96,8 @@ const { lchown } = require('fs/promises');
 // 6. Read the update.txt file and display the data. Additionally print a line "We are excited" using console.log() after the read operation
 
 // let a = fs.readFileSync('space_project/update.txt','utf-8')
-// console.log(a)
-// console.log('We are excited')
 
-// fs.readFile('space_project/update.txt',(err,data)=>{
+// fs.readFile('space_project/update.txt',(err,a)=>{
 //     if(err){
 //         throw err;
 //     }
@@ -68,7 +108,6 @@ const { lchown } = require('fs/promises');
 
 // 7. Delete the file update.txt
 
-// fs.unlinkSync('space_project/update.txt')
 // fs.unlink('space_project/update.txt',(err)=>{
 //     if(err)
 //     {
@@ -78,8 +117,6 @@ const { lchown } = require('fs/promises');
 
 
 // 8. Delete the folder space_project
-
-// fs.rmdirSync('space_project')
 
 // fs.rmdir('space_project',(err)=>{
 //  if(err){throw err}   
